3ae2854b07e1430b9021db56b4dd995c2e0a6326
zhanjixun <jixun-zhan@msyc.cc>
2020-09-21 16:34:15 +0800

    初始化

---
+++ src/main/java/com/github/zhanjixun/quickly/CollectorsExt.java
@@ -0,0 +1,34 @@
+package com.github.zhanjixun.quickly;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.function.Function;
+import java.util.stream.Collector;
+
+/**
+ * Collectors扩展
+ *
+ * @author zhanjixun
+ * @date 2020-09-21 16:25:01
+ */
+public class CollectorsExt {
+
+    /**
+     * 统计BigDecimal平均数
+     *
+     * @param mapper
+     * @param <T>
+     * @return
+     */
+    public static <T> Collector<T, ?, BigDecimal> averagingBigDecimal(Function<? super T, BigDecimal> mapper) {
+        return Collector.of(() -> new BigDecimal[]{new BigDecimal(0), new BigDecimal(0)}, (a, t) -> {
+            a[0] = a[0].add(mapper.apply(t));//累加
+            a[1] = a[1].add(BigDecimal.ONE);//计算个数
+        }, (a, b) -> {
+            //分组合并 合并总和
+            a[0] = a[0].add(b[0]);
+            return a;
+        }, a -> a[0].divide(a[1], MathContext.DECIMAL32));
+    }
+
+}
